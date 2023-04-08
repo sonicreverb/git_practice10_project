e554241d129a545eb1e88a9d8ca481dd9b654230
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2023-04-08 11:58:08 +0300

    Реализована функция проверки наличия пользователя в базе данных

--- SourceFiles/TeacherMode.cpp
+++ SourceFiles/TeacherMode.cpp
@@ -10,23 +10,56 @@ void TeacherAuthMenu()
 	TeacherData* teachers = new TeacherData[number_of_teachers];
 
 	teachers[0].login = "kazemir32";
-	teachers[1].login = "maxonpolyakov";
-	
-	TeacherData curr_user;
-	cout << "Добро пожаловать, для продолжения введите свой логин:" << endl;
-	getline(cin, curr_user.login);
-	
-	// проверка на существования логина в базе данных
+	teachers[0].name = "Каземир";
+	teachers[0].patronymic = "Каземирович";
 
-	bool flag_teacher_exist = false;
-	for (int arr_id = 0; arr_id < number_of_teachers; arr_id++)
+	teachers[1].login = "maxonpolyakov";
+	teachers[1].name = "Максим";
+	teachers[1].patronymic = "Козельскорожденнович";
+	
+	string state = "run";
+
+	do
 	{
-		if (teachers[arr_id].login == curr_user.login)
+		TeacherData curr_user;
+		int curr_user_arr_id_in_database;
+
+		cout << "Добро пожаловать, для продолжения введите свой логин (чтобы вернуться в главное меню введите 0):" << endl;
+		getline(cin, curr_user.login);
+
+		// пользователь хочет вернуться в главное меню
+		if (curr_user.login == "0") state = "shutdown";
+
+		// пользователь вводит логин
+		else
 		{
+			// проверка на существования логина в базе данных
+			bool flag_teacher_exist = false;
+			for (int arr_id = 0; arr_id < number_of_teachers; arr_id++)
+			{
+				if (teachers[arr_id].login == curr_user.login)
+				{
+					flag_teacher_exist = true;
+					curr_user_arr_id_in_database = arr_id;
+					break;
+				}
+			}
+
+			if (!flag_teacher_exist)
+			{
+				cout << "Извините, пользователь " << curr_user.login << " не найден.\n";
+				continue;
+			}
+
+			// пользователь существует
+			cout << "Вы входите, как " << teachers[curr_user_arr_id_in_database].name << " " << teachers[curr_user_arr_id_in_database].patronymic << ".\n";
+			cout << "Для продолжения введите пароль: ";
+			getline(cin, curr_user.login);
 
 		}
-	}
 
-	getchar();
+	
+	} while (state != "shutdown");
+	
 
 }
