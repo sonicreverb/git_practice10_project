13ac3f973e917f7201d86a84f62e86d5de8a436a
TimoshaMorozov <morozovtd1910@gmail.com>
2023-04-08 11:25:51 +0300

    Массив оценок

--- SourceFiles/StudentMode.cpp
+++ SourceFiles/StudentMode.cpp
@@ -8,8 +8,17 @@ using namespace std;
 
 void StudentAuthMenu()
 {
-	StudentAuthData curr_user;
+	StudentData curr_user;
 	cout << "Добро пожаловать, для продолжения введите свой логин:"<<endl;
+	
 
+	getline(cin, curr_user.login);
+	cout << curr_user.login;
+	getline(cin, curr_user.password);
+	cout << curr_user.password;
+	cout << "Здравствуйте, " << curr_user.name;
+
+		getchar();
+	
 }
 
