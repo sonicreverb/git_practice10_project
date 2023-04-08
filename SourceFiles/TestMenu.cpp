7222d96d43eaadd76052d25074574f023a8aef5a
Grigory Machehin <grigorymachehin@yandex.ru>
2023-04-08 14:00:11 +0300

    Treining mode update

--- SourceFiles/TestMenu.cpp
+++ SourceFiles/TestMenu.cpp
@@ -1,12 +1,52 @@
-#include <iostream>
-#include <string>
-
 #include "../Headers/TestMenuLib.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <locale>
 
 using namespace std;
 
-void TestMenu()
+void Training()
 {
-	cout << "тестовое меню";
-	getchar();
+    setlocale(LC_ALL, "en_US.UTF-8");
+    ifstream file("CircleTest.txt");
+
+    if (!file.is_open()) {
+        cout << "Ошибка открытия файла." << endl;
+        return;
+    }
+
+    srand(time(0));
+
+    int lines[10], range;
+    range = 30;
+    for (int i = 1; i < 11; i++)
+    {
+        lines[i] = rand() % range;
+    }
+    string line;
+    int digit, answer;
+    for (int i = 0; i < 10; i++) {
+        system("cls");
+        for (int j = 0; j < lines[i] - 1; j++) {
+            getline(file, line);
+        }
+        getline(file, line);
+        digit = line[line.length() - 1] - '0';
+        do
+        {
+            system("cls");
+            cout << line.substr(0, line.length() - 1) << endl;
+            cin >> answer;
+        } while (answer != digit);
+        cout << "ВЕРНО!";
+        system("pause");
+    }
+
+    file.close();
+
+    return;
 }
