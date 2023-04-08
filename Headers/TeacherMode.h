ad7df8237f81d39f322891496302914f12f148fd
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2023-04-08 10:47:43 +0300

    заглушка для Григория)

--- Headers/TeacherMode.h
+++ Headers/TeacherMode.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+struct TeacherAuthData
+{
+	string login;
+	string password;
+	string name_surname;
+};
+
+void TeacherAuthMenu();
---
+++ SourceFiles/TeacherMode.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "../Headers/TeacherMode.h"
+
+using namespace std;
+
+
+void TeacherAuthMenu()
+{
+	TeacherAuthData curr_user;
+	cout << "Добро пожаловать, для продолжения введите свой логин:" << endl;
+}
