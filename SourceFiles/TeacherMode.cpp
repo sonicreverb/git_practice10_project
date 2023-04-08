81e0b70de11a8073e96992bcf6b2580b927e1f40
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2023-04-08 11:36:10 +0300

    Init

--- SourceFiles/TeacherMode.cpp
+++ SourceFiles/TeacherMode.cpp
@@ -4,10 +4,15 @@
 
 using namespace std;
 
-
 void TeacherAuthMenu()
 {
-	TeacherAuthData curr_user;
+	int n = 2;
+	TeacherData* teachers = new TeacherData[n];
+
+	teachers[i].login;
+	
+
+	TeacherData curr_user;
 	cout << "Добро пожаловать, для продолжения введите свой логин:" << endl;
 	getchar();
 
