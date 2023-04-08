c4a02e1284ca85f01b8fe7aabf3ad81914a4a148
TimoshaMorozov <morozovtd1910@gmail.com>
2023-04-08 12:13:14 +0300

    menu1

--- Headers/StudentMode.h
+++ Headers/StudentMode.h
@@ -16,3 +16,5 @@ struct StudentData
 
 
 void StudentAuthMenu();
+
+void training();
--- SourceFiles/StudentMode.cpp
+++ SourceFiles/StudentMode.cpp
@@ -3,24 +3,45 @@
 #include "../Headers/StudentMode.h"
 using namespace std;
 
+void trainig() {
+	int test;
+	cout << "Введите номер теста:" << endl;
+	cout << "1) Циклы" << endl << "2) Массивы (одномерные и двумерные)" << endl << "3) Строки" << endl << "4) Рекурсия" << endl << "5) Файлы" << endl << "6) Адреса и указатели" << endl << "7) Динамическая память" << endl;
+	cin >> test;
+	switch (test)
+	{
+	case 1:; break;
+	case 2:; break;
+	case 3:; break;
+	case 4:; break;
+	case 5:; break;
+	case 6:; break;
+	case 7:; break;
+	case 0:; break;
 
+	}
+}
 
 
 void StudentAuthMenu()
 {
 	StudentData curr_user;
-	cout << "Добро пожаловать, для продолжения введите свой логин:"<<endl;
-	
-
+	int choose;
+	cout << "Добро пожаловать, для продолжения введите свой логин:" << endl;
 	getline(cin, curr_user.login);
 	cout << endl;
 	getline(cin, curr_user.password);
 	cout << endl;
-	cout << "Здравствуйте, " << curr_user.name<<endl;
-	
-
-
-		getchar();
-	
+	cout << "Здравствуйте, " << curr_user.name << endl;
+	cout << "Выберите род задания:" << endl << "1) Треннинг по теме" << endl << "Тестирование по теме" << endl << "Итоговый тест" << endl;
+	cin >> choose;
+	switch (choose)
+	{
+	case 1:trainig(); break;
+	case 2:; break;
+	case 3:; break;
+	}
+	getchar();
 }
+
 
