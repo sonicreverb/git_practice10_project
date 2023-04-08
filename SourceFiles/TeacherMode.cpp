8c9fd24b46d36e6f835be0d0a51eeaa0d9db21e7
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2023-04-08 11:42:03 +0300

    Update TeacherMode.cpp

--- SourceFiles/TeacherMode.cpp
+++ SourceFiles/TeacherMode.cpp
@@ -6,14 +6,27 @@ using namespace std;
 
 void TeacherAuthMenu()
 {
-	int n = 2;
-	TeacherData* teachers = new TeacherData[n];
+	int number_of_teachers = 2;
+	TeacherData* teachers = new TeacherData[number_of_teachers];
 
-	teachers[i].login;
+	teachers[0].login = "kazemir32";
+	teachers[1].login = "maxonpolyakov";
 	
-
 	TeacherData curr_user;
 	cout << "Добро пожаловать, для продолжения введите свой логин:" << endl;
+	getline(cin, curr_user.login);
+	
+	// проверка на существования логина в базе данных
+
+	bool flag_teacher_exist = false;
+	for (int arr_id = 0; arr_id < number_of_teachers; arr_id++)
+	{
+		if (teachers[arr_id].login == curr_user.login)
+		{
+
+		}
+	}
+
 	getchar();
 
 }
