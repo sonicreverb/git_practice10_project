bb1aa3148ef8c01bf20afb394f4c5d9792c8d8d0
Grigory Machehin <grigorymachehin@yandex.ru>
2023-04-12 23:05:07 +0300

    FinalTest update
    Добавлено итоговое тестирование( вопросы не повторяются и не соседствуют с другими вопросами из их исходного файла, выставляется оценка), откорректированы тренинг и тестирование по теме. Пока не реализован контроль ввода и взаимодействие с базой данных.

--- Headers/TestMenuLib.h
+++ Headers/TestMenuLib.h
@@ -11,3 +11,4 @@
 using namespace std;
 int TrainingOnTheme(string Theme);
 int TestingOnTheme(string Theme);
+int FinalTest();
--- SourceFiles/TestMenu.cpp
+++ SourceFiles/TestMenu.cpp
@@ -1,6 +1,5 @@
 #include "../Headers/TestMenuLib.h"
 
-
 int TrainingOnTheme(string Theme) {
     setlocale(LC_ALL, "ru_RU.UTF-8"); system("cls");
 
@@ -50,7 +49,6 @@ int TrainingOnTheme(string Theme) {
     setlocale(LC_ALL, "rus");
     return 0;
 }
-
 
 int TestingOnTheme(string Theme)
 {
@@ -133,13 +131,69 @@ int TestingOnTheme(string Theme)
     return 0;
 }
 
+int FinalTest() {
+    const int FILE_COUNT = 8; setlocale(LC_ALL, "ru_RU.UTF-8");
 
-int FinalTest()
-{
-    setlocale(LC_ALL, "ru_RU.UTF-8");
     system("cls");
-    //
-    //
+
+    ifstream cirFile("CircleTest.txt");
+    ifstream arrFile("ArrayTest.txt");
+    ifstream strFile("StringTest.txt");
+    ifstream recFile("RekursionTest.txt");
+    ifstream structFile("StructTest.txt");
+    ifstream filFile("FilesTest.txt");
+    ifstream poiFile("PointerTest.txt");
+    ifstream dinFile("DinMemory.txt");
+
+    vector<vector<string>> files = {
+        {},{},{},{},{},{},{},{}
+    };
+
+    vector<ifstream*> ifs = {
+        &cirFile, &arrFile, &strFile, &recFile, &structFile, &filFile, &poiFile, &dinFile
+    };
+
+    for (int i = 0; i < FILE_COUNT; i++) {
+        string line;
+        while (getline(*ifs[i], line)) {
+            files[i].push_back(line);
+        }
+        ifs[i]->close();
+    }
+
+    int correctAnswer = 0, answer = 0, mistakes = 0;
+    srand(time(nullptr));
+    set<int> chosenPositions;
+
+    for (int i = 0; i < 40; ++i) {
+        int fileNo = rand() % FILE_COUNT;
+        int pos;
+        do {
+            pos = rand() % files[fileNo].size();
+        } while (pos > 0 && files[fileNo][pos - 1].substr(0, 5) == files[fileNo][pos].substr(0, 5));
+
+        string& str = files[fileNo][pos];
+
+        if (!str.empty()) {
+            correctAnswer = str.back() - '0';
+        }
+        system("cls");
+        if (!str.empty()) {
+            cout << "QESTION #" << i + 1 << "  ";
+            cout << str.substr(0, str.size() - 1) << endl;
+        }
+        cout << " --> ";
+        cin >> answer;
+        if (answer == correctAnswer) cout << "CORRECT" << endl; else { cout << "WRONG ANSWER" << endl; mistakes++; }
+        system("pause");
+    }
+    system("cls");
+    if (mistakes > 15) cout << endl << endl << "Mark: 2(" << 40 - mistakes << "/40)" << endl;
+    else if (mistakes > 10) cout << endl << endl << "Mark: 3(" << 40 - mistakes << "/40)" << endl;
+    else if (mistakes > 5) cout << endl << endl << "Mark: 4(" << 40 - mistakes << "/40)" << endl;
+    else cout << endl << endl << "Mark: 5(" << 40 - mistakes << "/40)" << endl;
+
+    system("pause");
     system("cls");
     setlocale(LC_ALL, "rus");
     return 0;
