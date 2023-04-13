58c1b2545acefc8384932307d99c20b5adc9a59b
ProstoIlyushka <124445655+ProstoIlyushka@users.noreply.github.com>
2023-04-14 00:26:03 +0300

    Маленькое дополнение
    123 TB of child gay porn

--- Headers/FileReading.h
+++ Headers/FileReading.h
@@ -11,3 +11,8 @@ int CheckStudentListNumber();
 
 TeacherData* CheckTeacherList(TeacherData* teacherdata, int number);
 StudentData* CheckStudentList(StudentData* studentdata, int number);
+void TextDecryption(int number_of_text);//1 - student 2 - teacher 3 - test
+void TextEncryption(int number_of_text);//1 - student 2 - teacher 3 - test
+void AddStudent(StudentData* studentdata);//Just let me create these functions
+void DeleteStudent();
+void UpdateStudentBase(StudentData* studentdata);//Use this function after every test to save student's progress in file
