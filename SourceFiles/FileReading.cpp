7a818927388624f06268f230a2446aad049f801e
ProstoIlyushka <124445655+ProstoIlyushka@users.noreply.github.com>
2023-04-15 12:51:06 +0300

    Bruh
    M*therf*ckers

--- SourceFiles/FileReading.cpp
+++ SourceFiles/FileReading.cpp
@@ -1,10 +1,27 @@
 
-#include "../Headers/TeacherMode.h"
-#include "../Headers/StudentMode.h"
 #include "../Headers/FileReading.h"
 
 #define ABSPATH_TO_DATABASE "C:\\Users\\zaicz\\source\\repos\\Bruh10\\Bruh10\\git_practice10_project\\SourceFiles\\"
 
+
+void UpdateStudentBase(StudentData* studentdata, int number_of_students)// use this function after every test to save student's progress in file
+{
+	FILE* file;
+	int a = fopen_s(&file, ABSPATH_TO_DATABASE "students_database.txt", "wt");
+	fprintf(file, "%d\n", number_of_students);
+	for (int i = 0; i < number_of_students; i++)
+	{
+		fprintf(file, "\n%s\n", studentdata[i].login);
+		fprintf(file, "%s\n", studentdata[i].password);
+		fprintf(file, "%s\n", studentdata[i].surname);
+		fprintf(file, "%s\n", studentdata[i].name);
+		fprintf(file, "%s\n", studentdata[i].patronymic);
+		for (int j = 0; j < 9; j++)fprintf(file, "%d ", studentdata[i].marks[j]);
+		fprintf(file, "\n");
+		fprintf(file, "%d\n", studentdata[i].id);
+	}
+	/*TextEncryption(1);*/
+}
 
 int CheckTeacherListNumber()
 {
@@ -33,11 +50,10 @@ TeacherData* CheckTeacherList(TeacherData* teacherdata, int number)
 	for (int i = 0; i < number; i++)
 	{
 		teacherdata = (TeacherData*)realloc((void*)teacherdata, (i + 1) * sizeof(TeacherData));
-		fgets(string, 100, file);
 		fscanf_s(file, "%s", &teacherdata[i].login);
 		fscanf_s(file, "%s", &teacherdata[i].password);
-		fscanf_s(file, "%s", &teacherdata[i].name);
 		fscanf_s(file, "%s", &teacherdata[i].surname);
+		fscanf_s(file, "%s", &teacherdata[i].name);
 		fscanf_s(file, "%s", &teacherdata[i].patronymic);
 		fscanf_s(file, "%d", &teacherdata[i].numbers_of_students);
 		teacherdata[i].id_students_arr = (int*)malloc(teacherdata[i].numbers_of_students * sizeof(int));
@@ -56,22 +72,22 @@ StudentData* CheckStudentList(StudentData* studentdata, int number)
 	char string[100];
 	FILE* file;
 	int a = fopen_s(&file, ABSPATH_TO_DATABASE "students_database.txt", "rt");
-	fgets(string, 100, file);
+	fscanf_s(file, "%d%c", &number, &string[0]);
 	for (int i = 0; i < number; i++)
 	{
 		studentdata = (StudentData*)realloc((void*)studentdata, (i + 1) * sizeof(StudentData));
-		fgets(string, 100, file);
+		fscanf_s(file, "%c", &string[0]);
 		fscanf_s(file, "%s", &studentdata[i].login);
 		fscanf_s(file, "%s", &studentdata[i].password);
-		fscanf_s(file, "%s", &studentdata[i].name);
 		fscanf_s(file, "%s", &studentdata[i].surname);
+		fscanf_s(file, "%s", &studentdata[i].name);
 		fscanf_s(file, "%s", &studentdata[i].patronymic);
-		fscanf_s(file, "%d", &studentdata[i].id);
-		for (int j = 0; j < 11; j++)
+		for (int j = 0; j < 9; j++)
 		{
 			fscanf_s(file, "%d", &studentdata[i].marks[j]);
 		}
-		fgets(string, 100, file);
+		fscanf_s(file, "%d", &studentdata[i].id);
+		fscanf_s(file, "%c", &string[0]);
 	}
 	fclose(file);
 	return studentdata;
@@ -96,7 +112,7 @@ void TextDecryption(int number_of_text)//1 - student 2 - teacher 3 - test
 		i++;
 	} while (string[i - 1] > 0);
 	fclose(file);
-	for (int j = 0; j < i-1; j++)
+	for (int j = 0; j < i - 1; j++)
 	{
 		string[j]++;
 	}
@@ -151,7 +167,7 @@ void TextEncryption(int number_of_text)//1 - student 2 - teacher 3 - test
 
 void AddStudent(StudentData* studentdata, int* number_of_students)//Just let me create these functions
 {
-	(* number_of_students)++;
+	(*number_of_students)++;
 	studentdata = (StudentData*)realloc(studentdata, (*number_of_students) * sizeof(StudentData));
 	cout << "Введите фамилию студента: ";
 	cin >> studentdata[(*number_of_students) - 1].surname;
@@ -173,52 +189,32 @@ void AddStudent(StudentData* studentdata, int* number_of_students)//Just let me 
 	}
 	cout << "Введите пароль студента: ";
 	cin >> studentdata[(*number_of_students) - 1].password;
-	for(int i = 0;i<11;i++)studentdata[(*number_of_students) - 1].marks[i]=0;
+	for (int i = 0; i < 11; i++)studentdata[(*number_of_students) - 1].marks[i] = 0;
 	studentdata[(*number_of_students) - 1].id = studentdata[(*number_of_students) - 2].id + 1;
 	UpdateStudentBase(studentdata, *number_of_students);
 	return;
 }
 
-void DeleteStudent(StudentData* studentdata, int* number_of_students)
-{
-	string login;
-	cout << "Введите логин студента: ";
-	cin >> login;
-	int t = 0;
-	int delited_number;
-	for (int i = 0; i < (*number_of_students); i++)if (login == studentdata[i].login)
-	{
-		
-		t++;
-		delited_number = i;
-	}
-	if(!t)
-	{
-		cout << "Студент с данным логином не найден";
-		return;
-	}
-	for (int i = delited_number; i < (*number_of_students) - 1; i++)studentdata[i].login = studentdata[i + 1].login;
-	(*number_of_students)--;
-	studentdata = (StudentData*)realloc(studentdata, (*number_of_students) * sizeof(StudentData));
-	return;
-}
-
-void UpdateStudentBase(StudentData* studentdata, int number_of_students)// use this function after every test to save student's progress in file
-{
-	/*TextDecryption(1);*/
-	FILE* file;
-	int a = fopen_s(&file, ABSPATH_TO_DATABASE "students_database.txt", "wt");
-	fprintf(file, "%d\n", number_of_students);
-	for (int i = 0; i < number_of_students; i++)
-	{
-		fprintf(file, "\n%s\n", studentdata[i].login);
-		fprintf(file, "%s\n", studentdata[i].password);
-		fprintf(file, "%s\n", studentdata[i].name);
-		fprintf(file, "%s\n", studentdata[i].surname);
-		fprintf(file, "%s\n", studentdata[i].patronymic);
-		fprintf(file, "%d\n", studentdata[i].id);
-		for(int j=0; j < 11; j++)fprintf(file, "%d ", studentdata[i].marks[j]);
-		fprintf(file, "\n");
-	}
-	/*TextEncryption(1);*/
-}
+//void DeleteStudent(StudentData* studentdata, int* number_of_students)
+//{
+//	string login;
+//	cout << "Введите логин студента: ";
+//	cin >> login;
+//	int t = 0;
+//	int delited_number;
+//	for (int i = 0; i < (*number_of_students); i++)if (login == studentdata[i].login)
+//	{
+//
+//		t++;
+//		delited_number = i;
+//	}
+//	if (!t)
+//	{
+//		cout << "Студент с данным логином не найден";
+//		return;
+//	}
+//	for (int i = delited_number; i < (*number_of_students) - 1; i++)studentdata[i].login = studentdata[i + 1].login;
+//	(*number_of_students)--;
+//	studentdata = (StudentData*)realloc(studentdata, (*number_of_students) * sizeof(StudentData));
+//	return;
+//}
