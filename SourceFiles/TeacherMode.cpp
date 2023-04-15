833ade2c809f4f0a681de74e1bb26147b9391133
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2023-04-15 09:37:30 +0300

    Реализованы функции учителя, необходимо доделать вывод с сортировкой

--- SourceFiles/TeacherMode.cpp
+++ SourceFiles/TeacherMode.cpp
@@ -1,12 +1,15 @@
 #include "../Headers/TeacherMode.h"
 #include "../Headers/FileReading.h"
 
+#define DATABASE_LOCATION_VOLUME_NAME "D:"
+#define ABSPATH_TO_DATABASE "D:\\БГТУ\\Программирование\\Practice10_GroupProject\\git_practice10_project\\SourceFiles\\"
+
 int FileOutput(vector<string> lines)
 {
 	int i = 1;
 	for (auto& str : lines) {
 		if (!str.empty()) {
-			cout << "QESTION #" << i << "  ";
+			cout << "QUESTION #" << i << "  ";
 			cout << str.substr(0, str.size()) << endl;
 			i++;
 		}
@@ -195,25 +198,237 @@ void DelAndRegStudents()
 //TODO VLADIS
 void EditStudentsProgress()
 {
+	system("cls");
+	system(DATABASE_LOCATION_VOLUME_NAME);
+	system("cd " ABSPATH_TO_DATABASE "students_database.txt");
+}
+
+void ShowStudensList(int mode_id, StudentData* student_data, int students_quantity)
+{
+	system("cls");
+
+	switch (mode_id)
+	{
+	case(1):
+		// по всем темам
+		for (int student_num = 0; student_num < students_quantity; student_num++)
+		{
+			cout << "Пользователь #" << student_data[student_num].id << " " << student_data[student_num].login << endl;
+			cout << student_data[student_num].surname << " " << student_data[student_num].name << " " << student_data[student_num].patronymic << endl;
+			cout << "Оценки по всем темам:" << endl;
+
+			for (int mark_id = 0; mark_id < student_data[student_num].numbers_of_marks; mark_id++)
+			{
+				cout << student_data[student_num].marks[mark_id] << endl;
+			}
+			cout << "\n";
+		}
+		break;
+
+	case(2):
+		// определённый тест
+
+		int s_test_id;
+		double test_id;
+
+		cout << "Введите номер п/п теста:" << endl;
+		cin >> test_id;
+
+		while (test_id != round(test_id) || test_id <= 0)
+		{
+			cout << "Некорретное значение, повторите ввод:" << endl;
+			cin >> test_id;
+		} s_test_id = (int)test_id;
+
+		for (int student_num = 0; student_num < students_quantity; student_num++)
+		{
+			cout << "Пользователь #" << student_data[student_num].id << " " << student_data[student_num].login << endl;
+			cout << student_data[student_num].surname << " " << student_data[student_num].name << " " << student_data[student_num].patronymic << endl;
+			cout << "Тест #" << s_test_id << ":" << endl;
+
+			if (s_test_id > student_data->numbers_of_marks) 
+			{
+				cout << "Студент не выполнял тест #" << s_test_id << ".\n";
+			}
+			else
+			{
+				if (student_data[student_num].marks[s_test_id - 1] == 0) cout << "Студент не выполнял тест #" << s_test_id << ".\n";
+				else
+				{
+					cout << student_data[student_num].marks[s_test_id - 1] << endl;
+				}
+			}
+			cout << "\n";
+		}
+		break;
+
+	case(3):
+		// итоговый тест
+
+		for (int student_num = 0; student_num < students_quantity; student_num++)
+		{
+			cout << "Пользователь #" << student_data[student_num].id << " " << student_data[student_num].login << endl;
+			cout << student_data[student_num].surname << " " << student_data[student_num].name << " " << student_data[student_num].patronymic << endl;
+			cout << "Итоговый тест:" << endl;
+
+			if (student_data[student_num].marks[student_data[student_num].numbers_of_marks - 1] == 0) cout << "Студент не выполнял итоговый тест.\n";
+			else
+			{
+				cout << student_data[student_num].marks[student_data[student_num].numbers_of_marks - 1] << endl;
+			} 			
+			cout << "\n";
+		}
+		break;
+
+	case(4):
+		// средний бал
+
+		for (int student_num = 0; student_num < students_quantity; student_num++)
+		{
+			cout << "Пользователь #" << student_data[student_num].id << " " << student_data[student_num].login << endl;
+			cout << student_data[student_num].surname << " " << student_data[student_num].name << " " << student_data[student_num].patronymic << endl;
+			cout << "Средний бал:" << endl;
+			
+			double avg_mark = 0;
+			for (int mark_id = 0; mark_id < student_data[student_num].numbers_of_marks; mark_id++)
+			{
+				avg_mark += student_data[student_num].marks[mark_id];
+			} avg_mark = avg_mark / student_data[student_num].numbers_of_marks;
+
+			cout << avg_mark << endl;
+			cout << "\n";
+		}
+		break;
+	default:
+		break;
+	}
 
 }
 
-//TODO VLADIS
-void ShowStudensList()
+void MarksFilter(StudentData* student_data, int students_quanity)
 {
+	system("cls");
 
+	int* excellent_students_id = new int[students_quanity];
+	int excellent_students_quanity = 0;
+
+	int* good_students_id = new int[students_quanity];
+	int good_students_quanity = 0;
+
+	int* not_certified_students_id = new int[students_quanity];
+	int not_certified_quanity = 0;
+
+	for (int student_num = 0; student_num < students_quanity; student_num++)
+	{
+		bool is_excellent = true;
+		bool is_good = true;
+		bool is_not_certified = false;
+
+		for (int mark_id = 0; mark_id < student_data[student_num].numbers_of_marks; mark_id++)
+		{
+			if (student_data[student_num].marks[mark_id] == 4)
+			{
+				is_excellent = false;
+			};
+
+			if (student_data[student_num].marks[mark_id] == 3)
+			{
+				is_excellent = false;
+				is_good = false;
+			};
+
+			if (student_data[student_num].marks[mark_id] == 2)
+			{
+				is_excellent = false;
+				is_good = false;
+				is_not_certified = true;
+			};
+		}
+
+		if (is_not_certified) 
+		{
+			not_certified_students_id[not_certified_quanity] = student_num;
+			not_certified_quanity++;
+			continue;
+		}
+
+		if (is_excellent)
+		{
+			excellent_students_id[excellent_students_quanity] = student_num;
+			excellent_students_quanity++;
+			continue;
+		}
+
+		if (is_good)
+		{
+			good_students_id[good_students_quanity] = student_num;
+			good_students_quanity++;
+			continue;
+		}
+	}
+
+	cout << "Список отличников:" << endl;
+	if (excellent_students_quanity == 0) cout << "К сожалению, отличников не найдено...\n" << endl;
+	else
+	{
+		for (int student_num = 0; student_num < excellent_students_quanity; student_num++)
+		{
+			cout << student_data[excellent_students_id[student_num]].surname << " " << student_data[excellent_students_id[student_num]].name << " " << student_data[excellent_students_id[student_num]].patronymic << endl;
+			cout << "\n";
+		}
+	}
+	cout << "\n";
+
+	cout << "Список хорошистов:" << endl;
+	if (good_students_quanity == 0) cout << "К сожалению, хорошистов не найдено...\n" << endl;
+	else
+	{
+		for (int student_num = 0; student_num < good_students_quanity; student_num++)
+		{
+			cout << student_data[good_students_id[student_num]].surname << " " << student_data[good_students_id[student_num]].name << " " << student_data[good_students_id[student_num]].patronymic << endl;
+			cout << "\n";
+		}
+	}
+	cout << "\n";
+
+	cout << "Список неаттестованных студентов:" << endl;
+	if (good_students_quanity == 0) cout << "К счастью, неаттестованных студентов не найдено :).\n" << endl;
+	else
+	{
+		for (int student_num = 0; student_num < not_certified_quanity; student_num++)
+		{
+			cout << student_data[not_certified_students_id[student_num]].surname << " " << student_data[not_certified_students_id[student_num]].name << " " << student_data[not_certified_students_id[student_num]].patronymic << endl;
+			cout << "\n";
+		}
+	}
+	cout << "\n";
+
+
+	delete[] excellent_students_id;
+	delete[] good_students_id;
+	delete[] not_certified_students_id;
 }
 
-//TODO VLADIS
-void Filter()
+//TODO SOMEONE GUYS PLEASE HELP!!!!!!!!!
+void ShowSortList(StudentData* student_data, int students_quantity)
 {
+	for (int student_num = 0; student_num < students_quantity; student_num++)
+	{
+		cout << "Пользователь #" << student_data[student_num].id << " " << student_data[student_num].login << endl;
+		cout << student_data[student_num].surname << " " << student_data[student_num].name << " " << student_data[student_num].patronymic << endl;
+		cout << "Средний бал:" << endl;
 
-}
+		double avg_mark = 0;
+		for (int mark_id = 0; mark_id < student_data[student_num].numbers_of_marks; mark_id++)
+		{
+			avg_mark += student_data[student_num].marks[mark_id];
+		} avg_mark = avg_mark / student_data[student_num].numbers_of_marks;
 
-//TODO VLADIS
-void ShowSortList()
-{
+		student_data[student_num].avg_mark = avg_mark;
+	}
 
+
+	// ДОБАВИТЬ ВЫВОД ОТСОРТИРОВАННЫХ УЧЕНИКОВ ОТ БОЛЬШЕГО СРЕДНЕГО БАЛА К МЕНЬШЕМУ
 }
 
 void TeacherAuthMenu()
@@ -245,6 +460,67 @@ void TeacherAuthMenu()
 	teachers[1].patronymic = "Фенибутович";
 	teachers[1].password = "bobus2";
 	
+	int number_of_students = 4;
+	StudentData* students = new StudentData[number_of_students];
+
+	students[0].login = "student32";
+	students[0].name = "Максим";
+	students[0].surname = "Козлек";
+	students[0].patronymic = "Абрамович";
+	students[0].password = "qwerty";
+	students[0].id = 0;
+	students[0].numbers_of_marks = 6;
+	students[0].marks[0] = 2;
+	students[0].marks[1] = 3;
+	students[0].marks[2] = 4;
+	students[0].marks[3] = 5;
+	students[0].marks[4] = 0;
+	students[0].marks[5] = 2;
+
+
+	students[1].login = "demidovlox";
+	students[1].name = "Илья";
+	students[1].surname = "Зайцев";
+	students[1].patronymic = "Владимирович";
+	students[1].password = "qwerty";
+	students[1].id = 1;
+	students[1].numbers_of_marks = 6;
+	students[1].marks[0] = 5;
+	students[1].marks[1] = 5;
+	students[1].marks[2] = 5;
+	students[1].marks[3] = 5;
+	students[1].marks[4] = 5;
+	students[1].marks[5] = 5;
+
+	students[2].login = "robertorick";
+	students[2].name = "Ильяc";
+	students[2].surname = "Зайцев2";
+	students[2].patronymic = "Владимирович2";
+	students[2].password = "qwerty";
+	students[2].id = 1;
+	students[2].numbers_of_marks = 6;
+	students[2].marks[0] = 4;
+	students[2].marks[1] = 5;
+	students[2].marks[2] = 5;
+	students[2].marks[3] = 4;
+	students[2].marks[4] = 5;
+	students[2].marks[5] = 5;
+
+	students[3].login = "robertorickoooo";
+	students[3].name = "Ильяcsdsd";
+	students[3].surname = "Зайцев23";
+	students[3].patronymic = "Владимирович2";
+	students[3].password = "qwerty";
+	students[3].id = 1;
+	students[3].numbers_of_marks = 6;
+	students[3].marks[0] = 4;
+	students[3].marks[1] = 3;
+	students[3].marks[2] = 5;
+	students[3].marks[3] = 4;
+	students[3].marks[4] = 5;
+	students[3].marks[5] = 5;
+
+
 	string state = "run";
 
 	do
@@ -308,7 +584,7 @@ void TeacherAuthMenu()
 			if (attemps_left == 0)
 			{
 				cout << "Вы не авторизировались.\n";
-				cout << "Нажмите любую клавишу, чтобы продолжить.\n";
+				cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
 				getchar();
 				continue;
 			}
@@ -319,7 +595,6 @@ void TeacherAuthMenu()
 
 			do
 			{
-
 				system("cls");
 				curr_user = teachers[curr_user_arr_id_in_database];
 				cout << "Вы авторизированы, как " << curr_user.surname << " " << curr_user.name << " " << curr_user.patronymic << endl;
@@ -348,20 +623,51 @@ void TeacherAuthMenu()
 					{
 					case 1:
 						DelAndRegStudents();
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 						break;
 					case 2:
+						EditStudentsProgress();
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 						break;
 					case 3:
+						ShowStudensList(1, students, number_of_students);
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 						break;
 					case 4:
+						ShowStudensList(2, students, number_of_students);
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 						break;
 					case 5:
+						ShowStudensList(3, students, number_of_students);
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 						break;
 					case 6:
-						break;
+						ShowStudensList(4, students, number_of_students);
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 					case 7:
+						MarksFilter(students, number_of_students);
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
 						break;
 					case 8:
+						ShowSortList(students, number_of_students);
+						cout << "Нажмите клавишу ENTER, чтобы продолжить.\n";
+						getchar();
+						getchar();
+						break;
 						break;
 
 					default:
@@ -374,18 +680,14 @@ void TeacherAuthMenu()
 				}
 				default:
 				{
-					cout << "Вы ввели некорректное значение. Для продолжения нажмите любую клавишу" << endl;
-
+					cout << "Вы ввели некорректное значение. Для продолжения нажмите клавишу ENTER." << endl;
 					break;
 				}
 				}
 			} while (teacher_section_choose != 0);
-			
-			
-		}
 
-	
+		}
+			
 	} while (state != "shutdown");
 	
-
 }
